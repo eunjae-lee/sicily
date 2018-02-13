2a1cd4837029899ee163442df2ca19dfb53e6d4d
Paul Lee <paul@valuepotion.com>
2018-02-13 18:12:03 +0800

    Change notification message

--- lib/sicily/notify_util.rb
+++ lib/sicily/notify_util.rb
@@ -6,7 +6,7 @@ module Sicily
       msg = files.map { |file|
         File.basename(file)
       }.join(", ")
-      TerminalNotifier.notify(msg, :title => "Finished processing #{files.size} files")
+      TerminalNotifier.notify(msg, :title => "Finished processing #{files.size} file(s)")
     end
   end
 end
