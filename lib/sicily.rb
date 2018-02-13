7b66266ddc37ea68e9e796955b9ecbcb90f8e3af
Paul Lee <paul@valuepotion.com>
2018-02-13 18:27:01 +0800

    Don't monitor if any path is subfolder of another one.

--- lib/sicily.rb
+++ lib/sicily.rb
@@ -1,9 +1,32 @@
 require "sicily/version"
 require "sicily/config"
 require "sicily/monitor"
+require "pathname"
 
 module Sicily
+  @monitored_paths = []
+
   def self.on(path, &block)
+    if self.can_monitor?(@monitored_paths, path)
+      self.start_monitor(path, &block)
+    else
+      puts "cannot monitor : #{path}"
+    end
+  end
+
+  def self.start_monitor(path, &block)
+    @monitored_paths << File.expand_path(path)
     Monitor.new.on(path, &block)
+  end
+
+  def self.can_monitor?(prev_paths, new_path)
+    expanded_new_path = File.expand_path(new_path)
+    prev_paths.each do |prev_path|
+      is_sub_folder = Pathname.new(prev_path).fnmatch?(File.join(expanded_new_path, "**")) ||
+          Pathname.new(expanded_new_path).fnmatch?(File.join(prev_path, "**"))
+      return false if is_sub_folder
+    end
+
+    return true
   end
 end
