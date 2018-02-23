254d795c8fd913a99d90df8760d68e6d44c7676d
Paul Lee <paul@valuepotion.com>
2018-02-23 17:29:43 +0800

    Garden config_google

--- lib/sicily/config.rb
+++ lib/sicily/config.rb
@@ -1,12 +1,6 @@
 module Sicily
   class << self
-    attr_accessor :config, :config_google
-  end
-
-  def self.configure_google(&block)
-    # FIXME : This should go to somewhere in `google_photo_task.rb`
-    return if block.nil?
-    block.call Sicily.config_google
+    attr_accessor :config
   end
 
   class Config
@@ -21,10 +15,5 @@ module Sicily
     end
   end
 
-  class GoogleCredential
-    attr_accessor :id, :pw
-  end
+  Sicily.config = Config.new
 end
-
-Sicily.config = Sicily::Config.new
-Sicily.config_google = Sicily::GoogleCredential.new
--- lib/sicily/task/google_photo_task.rb
+++ lib/sicily/task/google_photo_task.rb
@@ -1,6 +1,21 @@
 require "sicily/config"
 
 module Sicily
+  class << self
+    attr_accessor :config_google
+  end
+
+  class GoogleCredential
+    attr_accessor :id, :pw
+  end
+
+  Sicily.config_google = GoogleCredential.new
+
+  def self.configure_google(&block)
+    return if block.nil?
+    block.call Sicily.config_google
+  end
+
   module Task
     module GooglePhotoTask
       def google_photo
