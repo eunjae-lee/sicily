32b837d67ff65ffc73a8571b7b0d96047381721c
Paul Lee <paul@valuepotion.com>
2018-02-27 14:48:21 +0800

    Add delay when detecting new file

--- lib/sicily/config.rb
+++ lib/sicily/config.rb
@@ -7,11 +7,13 @@ module Sicily
 
   class Config
     attr_reader :forbid_new_file_in_subfolder,
-                :num_thread_pool
+                :num_thread_pool,
+                :delay_on_file_monitoring
 
     def initialize
       @forbid_new_file_in_subfolder = true
       @num_thread_pool = 50
+      @delay_on_file_monitoring = 10
     end
   end
 
--- lib/sicily/monitor.rb
+++ lib/sicily/monitor.rb
@@ -6,18 +6,27 @@ require 'sicily/batch_processor'
 
 module Sicily
   class Monitor
-    def on(path, &block)
+    def on(path, &user_rule_block)
       path = validate_path!(path)
-      listener = Listen.to(path) do |_modified, added, _removed|
-        BatchProcessor.new(added).run(&block)
+      start_listener(path) do |files|
+        BatchProcessor.new(files).run(&user_rule_block)
       end
-      listener.start
     end
 
     def validate_path!(path)
       path = File.expand_path(path)
       raise "Unknown path : #{path}" unless File.exist?(path)
       path
+    end
+
+    private
+
+    def start_listener(path)
+      delay = Sicily.config.delay_on_file_monitoring
+      listener = Listen.to(path, wait_for_delay: delay) do |_modified, added, _removed|
+        yield added
+      end
+      listener.start
     end
   end
 end
