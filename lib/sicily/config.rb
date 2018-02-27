5d32cee1fbd36a73e0ff6f42299f2f90f4723a1b
Paul Lee <paul@valuepotion.com>
2018-02-27 23:37:43 +0800

    Consome all existing files on startup

--- lib/sicily/config.rb
+++ lib/sicily/config.rb
@@ -8,12 +8,14 @@ module Sicily
   class Config
     attr_reader :forbid_new_file_in_subfolder,
                 :num_thread_pool,
-                :delay_on_file_monitoring
+                :delay_on_file_monitoring,
+                :consume_on_start
 
     def initialize
       @forbid_new_file_in_subfolder = true
       @num_thread_pool = 50
       @delay_on_file_monitoring = 10
+      @consume_on_start = true
     end
   end
 
--- lib/sicily/monitor.rb
+++ lib/sicily/monitor.rb
@@ -8,6 +8,24 @@ require 'sicily/error/monitor_error'
 module Sicily
   class Monitor
     def on(path, &user_rule_block)
+      consume_all(path, &user_rule_block)
+      attach_monitor(path, &user_rule_block)
+    end
+
+    private
+
+    def consume_all(path, &user_rule_block)
+      return unless Sicily.config.consume_on_start
+
+      files = already_existing_files(path)
+      BatchProcessor.new(files).run(&user_rule_block)
+    end
+
+    def already_existing_files(path)
+      Dir["#{File.expand_path(path)}/**/*"]
+    end
+
+    def attach_monitor(path, &user_rule_block)
       Sicily.logger.info "Starting a monitor on #{path}"
       path = validate_and_expand_path(path)
       start_listener(path) do |files|
@@ -20,8 +38,6 @@ module Sicily
       raise MonitorError, "Monitor Failed. Unknown path : #{path}" unless File.exist?(path)
       path
     end
-
-    private
 
     def start_listener(path)
       delay = Sicily.config.delay_on_file_monitoring
