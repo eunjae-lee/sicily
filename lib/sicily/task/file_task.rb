de58a84eeecc6e992102cd1b5dbdf7e344136591
Paul Lee <paul@valuepotion.com>
2018-02-28 16:41:57 +0800

    Add log

--- lib/sicily/task/file_task.rb
+++ lib/sicily/task/file_task.rb
@@ -37,12 +37,14 @@ module Sicily
       def mv(path_dest)
         info "mv to #{path_dest}"
         path_dest = Base.prepare_dest_path(@path, path_dest)
+        info "      #{path_dest}"
         FileUtils.mv @path, path_dest
       end
 
       def cp(path_dest)
         info "cp to #{path_dest}"
         path_dest = Base.prepare_dest_path(@path, path_dest)
+        info "      #{path_dest}"
         FileUtils.cp @path, path_dest
       end
 
